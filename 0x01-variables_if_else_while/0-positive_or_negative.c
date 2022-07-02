74a1982dcedba424af5c6d0b6819219c49a38cd5
Ozoano <obiuchenna74@gmail.com>
2022-07-01 18:58:18 -0700

    Task 1

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,32 +1,30 @@
- #include <stdlib.h>
- #include <time.h>
- #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
-  /**
-   * main - assigns a random number to the value of n everytime
-   * it executes and print it
-   * Return: 0 (Success)
-   */
- 
-  int main(void)
-   {
-   int n;
+/**
+*main - assigns a random number to the value of n everytime
+*it executes and print it
+*Return: 0 (Success)
+*/ 
+int main(void)
+{
+int n;
 
-   srand(time(0));
-   n = rand() - RAND_MAX / 2;
- 
-   if (n > 0)
-   {
-   printf("%d is positive\n", n);
-   }
-   else if (n == 0)
-   {
-   printf("%d is zero\n", n);
-   }
-   else
-   {
-   printf("%d is negative\n", n);
-   }
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else
+{
+printf("%d is negative\n", n);
+}
 
-   return (0);
-   }
+return (0);
+}
--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,29 +3,29 @@
 #include <stdio.h>
 
 /**
-  * main -This prints the last digits of the random number in variable n
-  * Return: 0 (Success)
-  */
- int main()
- {
-   int n;
+* main -This prints the last digits of the random number in variable n
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
 
-   srand(time(0));
-   n = rand() - RAND_MAX / 2;
-   printf("Last digit of %d is %d\n", n, n%10);
-   if(n % 10 > 5)
-   {
-   printf("and is greater than 5\n");
-   }
-   else if(n % 10 == 0)
-   {
-   printf("and is 0\n");
-   }
-   else if(n % 10 < 6 && n % 10 != 0)
-   {
-   printf("and is less than 6 and not 0\n");
-   }
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+printf("Last digit of %d is %d\n", n, n % 10);
+if (n % 10 > 5)
+{
+printf("and is greater than 5\n");
+}
+else if (n % 10 == 0)
+{
+printf("and is 0\n");
+}
+else if (n % 10 < 6 && n % 10 != 0)
+{
+printf("and is less than 6 and not 0\n");
+}
 
-   return 0;
-   }
+return (0);
+}
 
