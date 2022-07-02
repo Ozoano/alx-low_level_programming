382bcac01434de39aed6196ccd10accc089f925d
Ozoano <obiuchenna74@gmail.com>
2022-07-01 21:52:59 -0700

    task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+* main -Entry points
+*This prints possible numbers of single digits
+*followed by a new line
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 48; n < 58; n++)
+{
+putchar(n);
+if (n != 57)
+{
+putchar(' ');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
+
+
