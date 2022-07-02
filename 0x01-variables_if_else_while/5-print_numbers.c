3d04bab9b07ef7f3dacaea26609bb47e3c5e5e64
Ozoano <obiuchenna74@gmail.com>
2022-07-01 21:15:26 -0700

    Task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main -Entry points
+*This prints all numbers from 0 to 9
+*followed by a new line
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int n;
+
+for (n = 0; n < 10; n++)
+{
+printf("%d", n);
+printf("\n");
+}
+return (0);
+}
+
+
