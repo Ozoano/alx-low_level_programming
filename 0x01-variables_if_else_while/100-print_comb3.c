dfaab8438ae948f5e0a27856aa5eacdd9faac627
Ozoano <obiuchenna74@gmail.com>
2022-07-01 22:14:58 -0700

    Task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+* main -Entry
+*Prints numbers from 00 to 89
+* Return: 0 (Success)
+*/
+int main(void)
+{
+int i, e;
+
+i = 48;
+e = 48;
+
+while (e <= 58)
+{
+i = 48;
+while (i < 58)
+{
+if (e != i && e < i)
+{
+putchar(e);
+putchar(i);
+if (i == 57 && e == 56)
+{
+break;
+}
+putchar(' ');
+putchar(' ');
+}
+i++;
+}
+e++;
+}
+putchar('\n');
+return (0);
+}
+
+
+
