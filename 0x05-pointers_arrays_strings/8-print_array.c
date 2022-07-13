40612507dd7aa0b0184b933da15c6e8f6edcb79d
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-13 01:20:54 +0100

    Update 8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -4,6 +4,7 @@
 *print_array - prints n elements of an array of integers,
 *followed by a new line.
 *@a:  The array
+*@n: The number of elements of the array to be printed
 */
 void print_array(int *a, int n)
 {
