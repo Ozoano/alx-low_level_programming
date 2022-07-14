ec531899ff4824986adf5d5df4ecbc758f93a72a
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-14 15:29:06 +0100

    Update 1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,13 +11,14 @@
 char *_strncat(char *dest, char *src, int n)
 {
 int destLen, i;
-  
+
 for (destLen = 0; dest[destLen] != '\0'; destLen++)
-  
+;
+
 for (i = 0; src[i] != '\0' && n > 0; i++, n--, destLen++)
 {
- dest[destLen] = src[i];
+dest[destLen] = src[i];
 }
-  
+
 return (dest);
 }
