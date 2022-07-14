9c7250d8a39abf67d6bae6c95d381c224c29c565
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-14 21:57:56 +0100

    Update 5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,13 +9,13 @@
 char *string_toupper(char *str)
 {
 int i = 0;
-  
-while ([i] != '\0')
+
+while (str[i] != '\0')
 {
 if (str[i] >= 'a' && str[i] <= 'z')
 str[i] -= ('a' - 'A');
 i++;
 }
-  
+
 return (str);
 }
