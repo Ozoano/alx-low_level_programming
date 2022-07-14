f8a76d9eee62db8cee10b169294e952882ed465c
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-14 20:11:02 +0100

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -2,26 +2,27 @@
 
 /**
  * _strcmp - Compares two strings.
- * @*s1: First string
- * @*s2: Second string
+ * @s1: First string
+ * @s2: Second string
  *
  * Return: 0 if similar, positive number if s1 > s2, otherwise negative number
  */
 int _strcmp(char *s1, char *s2)
 {
-while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+int i = 0;
+while( (s1 != '\0' && s2 != '\0' ) && s1 == s2 )
 {
 s1++;
 s2++;
 }
   
- if(*s1 == *s2)
+ if(s1 == *s2)
 {
- return 0; // strings are identical
+ return 0;
  }
   
  else
 {
- return *s1 - *s2;
+ return s1 - s2;
 }
 }
