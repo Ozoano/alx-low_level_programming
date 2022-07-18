bab6a11d2010fa1a6b6917ab22254a561fca11ea
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-18 13:10:37 +0100

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,13 +12,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i;
+int i;
 
-	for (i = 0; s[i] != '\0'; i++)
-	{
-		if (s[i] == c)
-			return (&s[i]);
-	}
+for (i = 0; s[i] != '\0'; i++)
+{
+if (s[i] == c)
+return (&s[i]);
+}
 
-	return (NULL);
+return (NULL);
 }
