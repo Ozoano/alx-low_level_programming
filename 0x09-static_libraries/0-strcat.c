f3bbe565663bb6f125de61bbb1cd15b03ace7317
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-07-22 14:13:42 +0100

    Create  0-strcat.c

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates two strings
+ * @dest: Destination string
+ * @src: Source string
+ *
+ * Return: A pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+int i;
+int j;
+
+for (i = 0; dest[i] != '\0'; i++)
+{
+}
