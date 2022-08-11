57147725f251ad82a714a2aa98c3ef8a6a28fd2a
Ozoano <106312924+Ozoano@users.noreply.github.com>
2022-08-12 00:19:35 +0100

    Update 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,22 +8,22 @@
  */
 size_t print_list(const list_t *h)
 {
-size_t nodes = 0;
+	size_t nodes = 0;
 
-while (h)
-{
-if (h->str == NULL)
-{
-printf("[0] (nil)\n");
-h = h->next;
-nodes++;
-continue;
-}
+	while (h)
+	{
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			nodes++;
+			continue;
+		}
 
-printf("[%d] %s\n", h->len, h->str);
-h = h->next;
-nodes++;
-}
+		printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		nodes++;
+	}
 
-return (nodes);
+	return (nodes);
 }
