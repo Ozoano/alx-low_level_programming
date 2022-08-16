5c427dcec377b1c4bdb7cba7974f35cc9ba609f2
Ozoano <obiuchenna74@gmail.com>
2022-08-16 14:03:46 -0700

    Task

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,11 +1,12 @@
 #include "lists.h"
+
 /**
  * print_listint - prints all the elements of a list.
- *
  * @h: head of a list
  *
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
 size_t  len = 0;
@@ -16,6 +17,5 @@ printf("%d\n", h->n);
 h = h->next;
 len++;
 }
-
 return (len);
 }
