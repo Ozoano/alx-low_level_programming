e9f48b0fd7573a34a2ca9a56202d8f40a7dcd991
Ozoano <obiuchenna74@gmail.com>
2022-08-18 13:08:46 -0700

    Bit manipulation

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointing to a string
- *
- * Return:  the converted numbe or zero
- */
+* binary_to_uint - converts a binary number to an unsigned int.
+* @b: pointing to a string
+*
+* Return:  the converted numbe or zero
+*/
 
 unsigned int binary_to_uint(const char *b)
 {
@@ -12,19 +12,18 @@ unsigned int i, res = 0;
 
 if (!b)
 return (0);
+for (i = 0; b[i]; i++)
+{
+if (b[i] < '0' || b[i] > '1')
+{
+return (0);
+}
+else
+{
+res = res << 1;
+res |= (b[i] - '0');
+}
+}
 
- for (i = 0; b[i]; i++)
- {
- if (b[i] < '0' || b[i] > '1')
- {
- return (0);
- }
- else
- {
- res = res << 1;
- res |= (b[i] - '0');
- }
- }
-
- return (res);
+return (res);
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,5 +11,5 @@ int get_bit(unsigned long int n, unsigned int index)
 if (index >= sizeof(n) * 8)
 return (-1);
 
-  return ((n >> index) & 1);
+return ((n >> index) & 1);
 }
