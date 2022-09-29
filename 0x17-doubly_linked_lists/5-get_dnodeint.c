f9c42d1ba1e863d3f2ee218fd399298c50f72811
git Ozoano <git obiuchenna74@gmail.com>
2022-09-29 10:14:41 -0700

    returns the nth node of a list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ *get_dnodeint_at_index - returns the nth node of a list
+ *@head: pointer to first node
+ *@index: node at nth position
+*
+* Return: node or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+unsigned int count = 0;
+if (head == NULL)
+	return (NULL);
+
+if (index == 0)
+	return (head);
+
+while (count < index)
+{
+	head = head->next;
+	count++;
+}
+return (head);
+}
