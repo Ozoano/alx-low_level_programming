b0e2e5cb1e802ff0af81c9ae2dece896eb72488d
git Ozoano <git obiuchenna74@gmail.com>
2022-09-30 11:15:07 -0700

    node deletion

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -13,12 +13,11 @@
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
 	dlistint_t *tmp = *head;
-	unsigned int i;
 
 	if (*head == NULL)
 		return (-1);
 
-	for (i = 0; i <= index - 1; i++)
+	for (; index != 1; index--)
 	{
 		if (tmp == NULL)
 			return (-1);
