f68e3b266e7290abc1971ed5bc525208e9d16427
git Ozoano <git obiuchenna74@gmail.com>
2022-09-29 19:58:27 -0700

    add at the beginning

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -12,21 +12,16 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 dlistint_t *new;
 
-if (head == NULL)
-	return (NULL);
-
 new = malloc(sizeof(dlistint_t));
 if (new == NULL)
 	return (NULL);
 
 new->n = n;
-new->next = *head;
 new->prev = NULL;
-*head = new;
-
+new->next = *head;
 if (*head != NULL)
-{
-(*head)->next = new;
-}
+	(*head)->prev = new;
+
+*head = new;
 return (new);
 }
