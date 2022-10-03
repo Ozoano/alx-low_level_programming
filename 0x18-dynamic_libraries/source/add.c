79b68f73e0158adb85618b425c6a7c9a7c5671ed
git Ozoano <git obiuchenna74@gmail.com>
2022-10-03 10:48:16 -0700

    100-operations.so

---
+++ 0x18-dynamic_libraries/source/add.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+/**
+ *add - adds two values
+ *@a: first value
+ *@b: second value
+ *
+ * Return: the value of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ *sub - subtract two values
+ *@a: first value
+*@b: second value
+*
+* Return: the difference
+*/
+
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+*mul - multiply two values
+*@a: first value
+*@b: second value
+* Return: the value of a and b
+*/
+int mul(int a, int b)
+{
+return (a *b);
+}
+
+/**
+*div - divides two value
+*@a: first value
+*@b: second value
+*
+*Return: the value of a and b
+*/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+*mod- modulos of two values
+*@a: first value
+*@b: second value
+*
+*Return: the value of a and b
+*/
+int mod(int a, int b)
+{
+	return (a % b);
+}
